5eee4ed729f58408467bb3f0c620f6b9676b412a
sumanastani <sumanastani@gmail.com>
2024-09-29 16:33:04 -0400

    implemented static and dynamic filtering using MappingJascksonValue

---
+++ src/main/java/com/learnsimon/rest/webservices/restful_web_service/filtering/someBean.java
@@ -0,0 +1,41 @@
+package com.learnsimon.rest.webservices.restful_web_service.filtering;
+
+import com.fasterxml.jackson.annotation.JsonFilter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@JsonFilter("SomeBeanFilter")
+public class someBean {
+    private String field1;
+
+//    @JsonIgnore
+    private String field2;
+
+    private String field3;
+
+    public someBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    @Override
+    public String toString() {
+        return "someBean{" +
+                "field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                '}';
+    }
+}
